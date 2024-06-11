c2613fdac401d18474f67b4951f0db617ab164ba
dawsw <123456789+odin@users.noreply.github.com>
2024-06-11 00:58:17 -0500

    Created basic math operator functions

---
+++ script.js
@@ -0,0 +1,21 @@
+function operate (operator, a, b) {
+    return operator(a, b);
+}
+
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+console.log(operate(divide, 3, 4))
