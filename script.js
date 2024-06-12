1a0fc05709d44f26cc1f20aad3dd5bb733e1a972
dawsw <123456789+odin@users.noreply.github.com>
2024-06-11 22:15:17 -0500

    Fixed clearDisplayNumbers function

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let numberArray = [0];
+
+
+
 function operate (operator, a, b) {
     return operator(a, b);
 }
@@ -18,4 +22,34 @@ function divide(a, b) {
     return a / b;
 }
 
-console.log(operate(divide, 3, 4))
+function displayNumber() {
+    let buttons = document.getElementsByName("number");
+    let displayNumbers = document.querySelector("#display-numbers");
+    
+    for (let button of buttons) {
+        button.addEventListener("click", () => {
+            displayNumbers.textContent += button.textContent;
+            numberArray.push(Number(button.textContent))
+        });
+
+        button.addEventListener("mousedown", function () {
+            button.style.backgroundColor = "rgb(77, 77, 77)";
+        });
+
+        button.addEventListener("mouseup", function () {
+            button.style.backgroundColor = "rgb(59, 59, 59)";
+        });
+    }
+}
+
+function clearDisplayNumbers() {
+    let button = document.getElementById("clearbutton");
+    let displayNumbers = document.querySelector("#display-numbers");
+
+    button.addEventListener("click", () => {
+        displayNumbers.textContent = "0";  
+    });
+}
+
+clearDisplayNumbers();
+displayNumber();
